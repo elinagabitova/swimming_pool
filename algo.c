41936c502efd48c52e13a607f898a4d5ea4e6ebd
∀lefskii <elinagabitovaa@gmail.com>
2019-08-19 22:56:32 +0300

    Update algo.c

--- algo.c
+++ algo.c
@@ -2,16 +2,27 @@ int algo(char **arr)
 {
   int i;
   int j;
+  int len;
   int **mas;
  
   i = 0;
   j = 0;
   
-  while (j != 
+  len = count(**arr);
+  while (j != )
 }
 
 int count (char **arr)
 {
   int i;
+  int len;
   
+  i = 0;
+  len = 0;
+  while (arr[i][1] != '\0')
+  {
+    len++;
+    i++;
+  }
+  return (len);
 }
