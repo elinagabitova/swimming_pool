f858b28f3331f9ba28951a01d3b0332b71da0f8f
∀lefskii <elinagabitovaa@gmail.com>
2019-08-20 15:50:18 +0300

    Update algo.c

--- algo.c
+++ algo.c
@@ -2,15 +2,15 @@ int algo(char **arr)
 {
   int i;
   int j;
-  int len;
+  char *sign;
   int **mas;
  
   i = 0;
   j = 0;
-  len = count(**arr); // проверить этот вызов
-  while (j != nmbr(arr))
+  sign = signs(arr[1]);
+  while (j != nmbr(arr[1][0]))
   {
-    while (i != len)
+    while (i != count(arr))
     {
       
       i++;
@@ -33,10 +33,9 @@ int count (char **arr)
   return (len);
 }
 
-int nmbr(char *str)
+int nmbr(char *str) // это для подсчета строк
 {
-  char num[20]; // это для подсчета строк
-  char sign[2];
+  char num[20]; 
   int i;
   int j;
   
@@ -47,17 +46,28 @@ int nmbr(char *str)
   while (i != 0)
   {
       num[i] = str[i];
-      while (j != 0) //отсчитываем последние три числа 
-      {
-        sign[j] = str[i];
-        j--;
-      }
       i--;
    }
   i = ft_atoi(num); // инициализировать ft_atoi.c
-  j = ft_atoi(sign);
-  return (i, j);
+  return (i); // это интовские переменные, а алгоритм принимает немного другое...
 }
+
+char signs(char *str)
+{
+  char sign[2];
+  int i;
+  int j;
   
-  
+  i = 0;
+  j = 2;
+  while (str[i] != '\0')
+    i++;
+  while (j != 0) //отсчитываем последние три числа 
+    {
+      sign[j] = str[i];
+      j--;
+      i--;
+    }
+  return (sign);
+}
   
